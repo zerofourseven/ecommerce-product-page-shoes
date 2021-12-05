ef7e33c36ebf92ab9a025f9820a6c69e2b9b3021
zerofourseven <cj315@hotmail.com>
2021-12-05 14:41:01 -0500

    Added navbar and sidebar components.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+/* Switches sidenav from expanded to non-expanded state. */
+function openSideNav() {
+  document.getElementById("sidenav").style.width = "30rem";
+  document.getElementById("shadow").style.visibility = "visible";
+ document.getElementById("shadow").style.opacity = "1";
+  
+}
+
+function closeSideNav() {
+  document.getElementById("sidenav").style.width = "0";
+ document.getElementById("shadow").style.visibility = "hidden";
+ document.getElementById("shadow").style.opacity = "0";
+}
+
+
+
+/* TODO code for shopping cart amount badge */
+
+/* TODO code for shopping cart display */
+
+/* TODO code for lightbox image gallery */
